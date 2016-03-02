42a9ddfc0c4ed2bd44cf5c55dcdb1673a6a9015f
andre.lopes@ua.pt <andre.lopes@ua.pt>
2016-03-02 18:51:51 +0000

    all opcodes implemented

--- src/main/java/Chip8.java
+++ src/main/java/Chip8.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * Created by Andre on 02/03/2016.
  */
@@ -8,8 +10,8 @@ public class Chip8 {
     private int I;
     private int sp;
     private int[] memory;
-    private char delay_timer;
-    private char sound_timer;
+    private byte delay_timer;
+    private byte sound_timer;
     private short[] stack;
     private int[] V;
     private short[] gfx;
@@ -85,11 +87,128 @@ public class Chip8 {
                 break;
 
             case 0x1000: // jumps to address NNN
-                pc = (short)(opcode & 0x0FFF);
+                pc = (short) (opcode & 0x0FFF);
                 break;
             case 0x3000: // skips the next instruction if VX equals NN
+                if (V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF))
+                    pc += 4;
+                else
+                    pc += 2;
+                break;
+
+            case 0x4000: // skips the next instruction if VX isn't equal to NN
+                if (V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF))
+                    pc += 4;
+                else
+                    pc += 2;
+                break;
+
+            case 0x5000: //skips the next instrction if VX equals VY
+                if (V[(opcode & 0x0F00) >> 8] == V[(opcode & 0x00F0) >> 4])
+                    pc += 4;
+                else
+                    pc += 2;
+                break;
+
+            case 0x6000: //Sets VX to NN.
+                V[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
+                pc += 2;
+                break;
+
+            case 0x7000: //Adds NN to VX.
+                V[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;
+                pc += 2;
+                break;
+
+            case 0x8000:
+                switch (opcode & 0x000F) {
+                    case 0x0000: // 0x8XY0: Sets VX to the value of VY
+                        V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
+                        pc += 2;
+                        break;
+
+                    case 0x0001: // 0x8XY1: Sets VX to "VX OR VY"
+                        V[(opcode & 0x0F00) >> 8] |= V[(opcode & 0x00F0) >> 4];
+                        pc += 2;
+                        break;
+
+                    case 0x0002: // 0x8XY2: Sets VX to "VX AND VY"
+                        V[(opcode & 0x0F00) >> 8] &= V[(opcode & 0x00F0) >> 4];
+                        pc += 2;
+                        break;
+
+                    case 0x0003: // 0x8XY3: Sets VX to "VX XOR VY"
+                        V[(opcode & 0x0F00) >> 8] ^= V[(opcode & 0x00F0) >> 4];
+                        pc += 2;
+                        break;
+
+                    case 0x0004: // 0x8XY4: Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't
+                        if (V[(opcode & 0x00F0) >> 4] > (0xFF - V[(opcode & 0x0F00) >> 8]))
+                            V[0xF] = 1; //carry
+                        else
+                            V[0xF] = 0;
+                        V[(opcode & 0x0F00) >> 8] += V[(opcode & 0x00F0) >> 4];
+                        pc += 2;
+                        break;
+
+                    case 0x0005: // 0x8XY5: VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't
+                        if (V[(opcode & 0x00F0) >> 4] > V[(opcode & 0x0F00) >> 8])
+                            V[0xF] = 0; // there is a borrow
+                        else
+                            V[0xF] = 1;
+                        V[(opcode & 0x0F00) >> 8] -= V[(opcode & 0x00F0) >> 4];
+                        pc += 2;
+                        break;
+
+                    case 0x0006: // 0x8XY6: Shifts VX right by one. VF is set to the value of the least significant bit of VX before the shift
+                        V[0xF] = V[(opcode & 0x0F00) >> 8] & 0x1;
+                        V[(opcode & 0x0F00) >> 8] >>= 1;
+                        pc += 2;
+                        break;
+
+                    case 0x0007: // 0x8XY7: Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't
+                        if (V[(opcode & 0x0F00) >> 8] > V[(opcode & 0x00F0) >> 4])    // VY-VX
+                            V[0xF] = 0; // there is a borrow
+                        else
+                            V[0xF] = 1;
+                        V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4] - V[(opcode & 0x0F00) >> 8];
+                        pc += 2;
+                        break;
+
+                    case 0x000E: // 0x8XYE: Shifts VX left by one. VF is set to the value of the most significant bit of VX before the shift
+                        V[0xF] = V[(opcode & 0x0F00) >> 8] >> 7;
+                        V[(opcode & 0x0F00) >> 8] <<= 1;
+                        pc += 2;
+                        break;
+
+                    default:
+                        System.out.printf("Unknown opcode [0x8000]: 0x%X\n", opcode);
+                }
+                break;
+
+            case 0x9000: // Skips the next instruction if VX doesn't equal VY.
+
+                if (V[(opcode & 0x0F00) >> 8] != V[(opcode & 0x00F0) >> 4])
+                    pc += 4;
+                else
+                    pc += 2;
+                break;
+
+
             case 0xA000: //ANNN: Setts I to the address NNN
                 I = opcode & 0x0FFF;
+                pc += 2;
+                break;
+
+            case 0xB000: //Jumps to the address NNN plus V0.
+                pc = (short) ((opcode & 0xFFF) + V[0]);
+
+                break;
+            case 0xC000: // CXNN: Sets VX to a random number and NN
+
+                Random rand = new Random();
+                int r = rand.nextInt(255);
+                V[(opcode & 0x0F00) >> 8] = (r % 0xFF) & (opcode & 0x00FF);
                 pc += 2;
                 break;
 
@@ -149,18 +268,100 @@ public class Chip8 {
                         else
                             pc += 2;
                         break;
-                    default:
-                        System.out.printf("Unknown opcode: 0x%X\n", opcode);
-                }
+                    case 0x00A1: // EXA1: Skips the next instruction if the key stored in VX isn't pressed
+                        if (key[V[(opcode & 0x0F00) >> 8]] == 0)
+                            pc += 4;
+                        else
+                            pc += 2;
+                        break;
 
-                // Update timers
-                if (delay_timer > 0)
-                    --delay_timer;
+                    case 0xF000:
+                        switch (opcode & 0x00FF) {
+                            case 0x0007: // FX07: Sets VX to the value of the delay timer
+                                V[(opcode & 0x0F00) >> 8] = delay_timer;
+                                pc += 2;
+                                break;
 
-                if (sound_timer > 0) {
-                    if (sound_timer == 1)
-                        System.out.printf("BEEP\n");
-                    --sound_timer;
+                            case 0x000A: // FX0A: A key press is awaited, and then stored in VX
+                            {
+                                boolean keyPress = false;
+
+                                for (int i = 0; i < 16; ++i) {
+                                    if (key[i] != 0) {
+                                        V[(opcode & 0x0F00) >> 8] = i;
+                                        keyPress = true;
+                                    }
+                                }
+
+                                // If we didn't received a keypress, skip this cycle and try again.
+                                if (!keyPress)
+                                    return;
+
+                                pc += 2;
+                            }
+                            break;
+
+                            case 0x0015: // FX15: Sets the delay timer to VX
+                                delay_timer = (byte) V[(opcode & 0x0F00) >> 8];
+                                pc += 2;
+                                break;
+
+                            case 0x0018: // FX18: Sets the sound timer to VX
+                                sound_timer = (byte) V[(opcode & 0x0F00) >> 8];
+                                pc += 2;
+                                break;
+
+                            case 0x001E: // FX1E: Adds VX to I
+                                if (I + V[(opcode & 0x0F00) >> 8] > 0xFFF)    // VF is set to 1 when range overflow (I+VX>0xFFF), and 0 when there isn't.
+                                    V[0xF] = 1;
+                                else
+                                    V[0xF] = 0;
+                                I += V[(opcode & 0x0F00) >> 8];
+                                pc += 2;
+                                break;
+
+                            case 0x0029: // FX29: Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font
+                                I = V[(opcode & 0x0F00) >> 8] * 0x5;
+                                pc += 2;
+                                break;
+
+                            case 0x0033: // FX33: Stores the Binary-coded decimal representation of VX at the addresses I, I plus 1, and I plus 2
+                                memory[I] = V[(opcode & 0x0F00) >> 8] / 100;
+                                memory[I + 1] = (V[(opcode & 0x0F00) >> 8] / 10) % 10;
+                                memory[I + 2] = (V[(opcode & 0x0F00) >> 8] % 100) % 10;
+                                pc += 2;
+                                break;
+
+                            case 0x0055: // FX55: Stores V0 to VX in memory starting at address I
+                                for (int i = 0; i <= ((opcode & 0x0F00) >> 8); ++i)
+                                    memory[I + i] = V[i];
+
+                                // On the original interpreter, when the operation is done, I = I + X + 1.
+                                I += ((opcode & 0x0F00) >> 8) + 1;
+                                pc += 2;
+                                break;
+
+                            case 0x0065: // FX65: Fills V0 to VX with values from memory starting at address I
+                                for (int i = 0; i <= ((opcode & 0x0F00) >> 8); ++i)
+                                    V[i] = memory[I + i];
+
+                                // On the original interpreter, when the operation is done, I = I + X + 1.
+                                I += ((opcode & 0x0F00) >> 8) + 1;
+                                pc += 2;
+                                break;
+                            default:
+                                System.out.printf("Unknown opcode: 0x%X\n", opcode);
+                        }
+
+                        // Update timers
+                        if (delay_timer > 0)
+                            --delay_timer;
+
+                        if (sound_timer > 0) {
+                            if (sound_timer == 1)
+                                System.out.printf("BEEP\n");
+                            --sound_timer;
+                        }
                 }
         }
     }
